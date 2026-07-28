057f98e0fc3e323acaa1597f2af80374672cd8d0
franUPB <francisco.castanov@upb.edu.co>
2026-07-28 13:35:34 -0500

    Mi primer commit

---
+++ 1.VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1.VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre="Francisco";
+            Console.WriteLine("Bienvenido " + nombre + " al curso Fundamentos de Programación");
+        }
+    }
+}
