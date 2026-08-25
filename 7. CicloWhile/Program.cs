04821722119f85748e32928e55ca254d7a575424
Liden <francisco.castanov@upb.edu.co>
2026-08-25 13:31:29 -0500

    7.CicloWhile

---
+++ 7. CicloWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _7.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Escribir un algoritmo que permita generar los primeros 5 números enteros positivos y realizar y mostrar su suma.
+            int contador = 1;
+            int acumulador = 1;
+
+            while (contador < 5)
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros números es: {acumulador}");
+
+        }
+    }
+}
