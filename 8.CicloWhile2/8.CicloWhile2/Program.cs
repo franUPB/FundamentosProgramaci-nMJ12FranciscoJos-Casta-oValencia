77468dfa4400e4c044816b5f7f675b721c3d0c18
Liden <francisco.castanov@upb.edu.co>
2026-08-27 13:32:40 -0500

    8.CicloWhile2

---
+++ 8.CicloWhile2/8.CicloWhile2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8.CicloWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumaEnteros = 0;
+            int numero = 0;
+            Console.WriteLine("Ingrese un número para sumar:");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0) 
+            {
+                sumaEnteros += numero;
+                Console.WriteLine("Ingrese un número para sumar:");
+                numero = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"La suma de los números es: {sumaEnteros}");
+
+
+        }
+    }
+}
