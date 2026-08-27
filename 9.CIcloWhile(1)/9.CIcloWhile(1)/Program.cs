528e8e821994316adaf0e20bd0862f698d2d1a99
Liden <francisco.castanov@upb.edu.co>
2026-08-27 13:33:25 -0500

    9.CicloWhile

---
+++ 9.CIcloWhile(1)/9.CIcloWhile(1)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _9.CIcloWhile_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int cantidadNumeros = 0;
+            //int contador = 0;
+            //int numero = 0;
+            //int contadorCeros = 0;
+            //int contadorPositivos = 0;
+            //int contadorNegativos = 0;
+
+
+            //Console.WriteLine("Cuál es la cantidad de números a evaluar");
+            //cantidadNumeros = int.Parse(Console.ReadLine());
+
+
+            //while (contador <= cantidadNumeros)
+            //{
+            //    contador++;
+            //    Console.WriteLine("Ingrese el número a evaluar");
+            //    numero = int.Parse(Console.ReadLine());
+
+            //    if (numero == 0)
+            //    {
+            //        contadorCeros++;
+            //    }
+            //    else
+            //    {
+            //        if (numero > 0)
+            //        {
+            //            contadorPositivos++;
+            //        }
+            //        else
+            //        {
+            //            contadorNegativos++;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine($"Mayores que 0: {contadorPositivos}, menores que cero: {contadorNegativos}, iguales a cero: {contadorCeros}");
+
+            //Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n. (operador mod en c#)
+
+            int numero = 0;
+            int contador = 0;
+
+            Console.WriteLine("ingrese un número cualquiera");
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero) 
+            {
+            }
+        }
+    }
+}
