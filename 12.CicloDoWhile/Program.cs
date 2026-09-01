d8effca39b0555e4f3cdf501f3fdf4ee559fe69e
Liden <francisco.castanov@upb.edu.co>
2026-09-01 13:29:22 -0500

    12.CicloDoWhile

---
+++ 12.CicloDoWhile/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _12.CicloDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Ciclo Do While
+            //int contador = 1;
+            //int acumulador = 0;
+
+            //do
+            //{
+            //    acumulador += contador;
+            //        contador++;
+            //} while (contador <= 5);
+
+            //Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+
+            //Algoritmo que solicite un número y genere su correspondiente tabla de multiplicar desde el 1 hasta el 10. Y así sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar
+
+            //int num = 0;
+            //int contador = 1;
+            //char respuesta = ' ';
+            
+
+            //do
+            //{
+            //        Console.WriteLine("Ingrese un número entre 1 y 10");
+            //        num = int.Parse(Console.ReadLine ());
+            //    do
+            //    {
+                    
+            //        Console.WriteLine($"{num} x {contador}={num * contador}");
+            //        contador++;
+
+            //    } while (contador <= 10);
+            //    contador = 1;
+            //    Console.WriteLine("Desea continuar: s/sí - Cualquier otro caracter para salir");
+            //    respuesta = char.Parse (Console.ReadLine ());
+            //} while (respuesta == 's');
+
+
+            //Crear un algoritmo que permita calcular y mostrar por pantalla los números primos hasta el número 100.
+
+            int contador = 0;
+            do
+            {
+
+
+            } while (contador <= 100); 
+        }
+    }
+}
