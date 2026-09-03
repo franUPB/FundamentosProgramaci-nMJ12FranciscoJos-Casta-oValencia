a9fe0cf8cf05daee6694abfbecc73cc53ab64bf9
Liden <francisco.castanov@upb.edu.co>
2026-09-03 13:26:22 -0500

    13. CicloFor

---
+++ 13. CicloFor/13. CicloFor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _13.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclo For
+            //int acumulador = 0;
+
+            //for(int contador = 1; contador <= 5; contador ++)
+            //{
+
+            //    acumulador += contador;
+
+            //}
+            //Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+
+
+
+
+            //Algoritmo que le permita pedirle un número al usuario y calcular la factorial del número ingresado.
+
+            int num = 0;
+            int acumulador = 0;
+
+            Console.WriteLine("Ingrese un número:");
+            num = int.Parse(Console.ReadLine());
+
+            for (int contador = 0;  contador < num; contador++)
+            {
+
+            }
+        }
+    }
+}
