c22a233010a3f1fe2fb3467b95eabf7aab8b993b
Liden <francisco.castanov@upb.edu.co>
2026-09-09 22:11:46 -0500

    14.TallerCiclos

--- 14.TallerCiclos/Program.cs
+++ 14.TallerCiclos/Program.cs
@@ -8,7 +8,7 @@ namespace _14.TallerCiclos
         static void Main(string[] args)
         {
             //1. Algoritmo que permita calcular el promedio de calificaciones, el algoritmo le permitirá al usuario, introducir tantas calificaciones como así desee, en el momento en que seleccione que no desea continuar capturando calificaciones, el algoritmo debe presentar el promedio de las calificaciones capturadas previamente. 
-            
+
             //int calificacion = 0;
             //string opción = "s";
             //float acumulador = 0;
@@ -29,46 +29,95 @@ namespace _14.TallerCiclos
             //Console.WriteLine($"el promedio de las calificaciones es: {acumulador / contador}");
 
 
+            //2.
+
+            //int numero = 0;
+
+            //Console.WriteLine("Ingrese un número:");
+            //numero = int.Parse(Console.ReadLine());
+
+            //for (int v = 1; v <= numero; v++)
+            //{
+
+            //    if (numero % v == 0)
+            //    {
+            //        Console.WriteLine(v);
+            //    }
+
+            //}
+
+            //3. 
+
+            //    int b = 0;
+            //int e = 0;
+            //string opcion = "s";
+            //do
+            //{
+
+            //    Console.WriteLine("ingrese un número:");
+            //    b = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("ingrese una potencia para el número:");
+            //    e = int.Parse(Console.ReadLine());
+
+            //    double resultado = Math.Pow(b, e);
+
+            //    Console.WriteLine($"la potencia del número {b} es: {resultado}");
+
+            //    Console.WriteLine("¿desea calcular otra potencia? s/sí, cualquier otra letra para no");
+            //    opcion = Console.ReadLine();
+
+            //} while (opcion == "s");
 
 
+            //4.
+
+            int tiempoPrueba = 0;
+            int tiempoTotal = 0;
+            int cantidadPruebas = 1;
+            float promedio = 0;
+            bool apto = true;
+            bool pruebaCumplida = false;
 
 
-
-
-
-
-
-            //3. Dados dos números enteros ingresados por teclado: b que es la base y 
-            //     e que es el exponente, se requiere calcular el resultado de la
-            //     potenciación.
-            //     Ejemplo: b = 2, e = 5  25 = 2 * 2 * 2 * 2 * 2 = 32
-            //     Mostrar por pantalla el resultado de la potenciación. 
-            //     Seguir pidiendo por teclado la base y el exponente y realizar la
-            //     potenciación correspondiente, hasta que el usuario ingrese por teclado
-            //     el carácter de escape ‘n’
-
-            int b = 0;
-            int e = 0;
-            string opcion = "s";
             do
             {
+                Console.WriteLine($"Ingrese el tiempo del dia {cantidadPruebas}");
+                tiempoPrueba = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("ingrese un número:");
-                b = int.Parse(Console.ReadLine());
-                Console.WriteLine("ingrese una potencia para el número:");
-                e = int.Parse(Console.ReadLine());
+                tiempoTotal = tiempoTotal + tiempoPrueba;
+                promedio = tiempoTotal / cantidadPruebas;
 
-                double resultado = Math.Pow(b, e);
+                if (tiempoPrueba > 20)
+                {
+                    apto = false;
+                }
+                if (tiempoPrueba < 15)
+                {
+                    pruebaCumplida = true;
+                }
+                if (promedio > 18)
+                {
+                    apto = false;
+                }
 
-                Console.WriteLine($"la potencia de número {b} es: {resultado}");
+                cantidadPruebas++;
+            } while (apto && cantidadPruebas <= 10);
 
-                Console.WriteLine("¿desea calcular otra potencia? s/sí, cualquier otra letra para no");
-                opcion = Console.ReadLine();
+            Console.WriteLine($" promedio es: {promedio}");
 
-            } while (opcion == "s");
-            
+            if (!pruebaCumplida)
+            {
+                apto = false;
+            }
 
-
+            if (apto)
+            {
+                Console.WriteLine("El atleta es apto");
+            }
+            else
+            {
+                Console.WriteLine("El atleta no es apto");
+            }
 
         }
     }
